3700314ca62df1a9f95c72b4bb1780468e8f8188
rokserak <rok.serak@gmail.com>
2019-08-10 17:32:05 +0200

    setters, getters, create

--- database.sol
+++ database.sol
@@ -1,20 +1,54 @@
 pragma solidity ^0.5.10;
 
 contract database {
-    struct object {
+    struct Object {
         //atributes
-        int id;
         int number;
         string text;
         bool is_deleted; //default = false
     }
 
     int id = 0;
-    mapping (int => int) list;
+    mapping (int => Object) list;
 
-    //methods
-    function auto_increment() public {
-        id = id + 1;
+    //each id is unique
+    function auto_increment() internal {
+        id++;
     }
 
+    //setters
+    function set_number(int idd, int x) internal {
+        if (!list[idd].is_deleted) {
+            list[idd].number = x;
+        }
+    }
+
+    function set_text(int idd, string memory x) internal {
+        if (!list[idd].is_deleted) {
+            list[idd].text = x;
+        }
+    }
+
+    function deleted(int idd) internal {
+        list[idd].is_deleted = true;
+    }
+
+    //getters
+    function get_number(int idd) public view returns (int) {
+        return list[idd].number;
+    }
+
+    function get_text(int idd) public view returns (string memory) {
+        return list[idd].text;
+    }
+
+    //create - append
+    function create(int n, string memory t) public {
+        list[id] = Object({
+            number: n,
+            text: t,
+            is_deleted: false
+        });
+        auto_increment();
+    }
 }
